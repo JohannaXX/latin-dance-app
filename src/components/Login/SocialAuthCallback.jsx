f9393eb9a58698a447d396d1d75dfbebc904df37
Johanna <2myadverts@gmail.com>
2020-11-06 21:20:22 +0100

    slack login

---
+++ src/components/Login/SocialAuthCallback.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import { useAuthContext } from '../contexts/AuthContext'
+import { queryParams } from '../helpers/helpers'
+import { socialLogin } from '../services/ApiClient'
+
+const SocialAuthCallback = () => {
+  const { login } = useAuthContext()
+
+  useEffect(() => {
+    const { code } = queryParams()
+
+    socialLogin(code)
+      .then(user => {
+        login(user)
+      })
+  }, [login])
+
+  return null
+}
+
+export default SocialAuthCallback;
