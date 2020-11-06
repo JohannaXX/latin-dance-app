cf71d323c0957c766c9d1113bcf020ca89d0981f
Johanna <2myadverts@gmail.com>
2020-11-06 21:48:53 +0100

    slack

--- src/components/Login/SocialAuthCallback.jsx
+++ src/components/Login/SocialAuthCallback.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useAuthContext } from '../../contexts/AuthContext'
-import { queryParams } from '../../helpers/helpers'
+import { queryParams } from '../../helpers/helper'
 import { socialLogin } from '../../services/UserClient'
 
 const SocialAuthCallback = () => {
